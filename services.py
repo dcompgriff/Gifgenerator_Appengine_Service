067f6d35b0213ab9d4daadb946ab771f1fe78392
PR <unknown7979@gmail.com>
2016-02-24 16:53:15 -0500

    Implemented extra service endpoionts, and serving of index template.

--- services.py
+++ services.py
@@ -1,10 +1,23 @@
 import webapp2
+import jinja2
+import os
+import urllib
+
+JINJA_ENVIRONMENT = jinja2.Environment(loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),extensions=['jinja2.ext.autoescape'], autoescape=True)
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+		template = JINJA_ENVIRONMENT.get_template('index.html')
+		template_values = {}
+		self.response.write(template.render(template_values))
+
+class SumSquaredSequence(webapp2.RequestHandler):
+	def get(self):
+		self.response.write("Called sum squared text")
+		
+
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    (r'/', MainPage),
+	(r'/sumsqnum/', SumSquaredSequence),
 ], debug=True)
